f2daff2fe2c033d8e714e30a63d013abd3ccd036
Max Miroshnichenko <foxmax@list.ru>
2024-12-29 16:03:05 +0300

    Initial

---
+++ Course for Advanced/Module_14_turtle/task_14_2_12_snowflakes.py
@@ -0,0 +1,42 @@
+from turtle import *
+import random
+
+
+def snowflakes(scale, x, y):
+    segment = 10 * scale
+    speed(10)
+    penup()
+    goto(x, y)
+    pendown()
+    r, g, b = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+    pencolor(r, g, b)
+    pensize(4)
+    for _ in range(8):
+        for _ in range(3):
+            fd(segment)
+            left(45)
+            fd(segment)
+            bk(segment)
+            right(90)
+            fd(segment)
+            bk(segment)
+            left(45)
+        fd(segment)
+        bk(segment * 4)
+        left(45)
+
+
+x, y = -300, 300
+colormode(255)
+bgcolor(145, 152, 231)
+for _ in range(4):
+    scale = random.randint(1, 5)
+    snowflakes(scale, x, y)
+    penup()
+    fd(600)
+    pendown()
+    x, y = pos()
+    right(90)
+snowflakes(2, 0, 0)
+
+mainloop()
