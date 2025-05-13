adcca1220c80568e494bcbef4e9ec1c3189d21d3
Max Miroshnichenko <foxmax@list.ru>
2025-05-13 18:08:39 +0300

    Initial

---
+++ Course for Profi/7 exceptions/ex_7-2-1.py
@@ -0,0 +1,22 @@
+'''
+Вам доступна программа, которая находит сумму всех значений по ключу Likes из всех словарей списка
+blog_posts. Если словарь не содержит ключа Likes, его значение считается равным минус единице. Дополните
+приведенный ниже код конструкцией try-except, чтобы он выполнился без ошибок.
+'''
+
+blog_posts = [{'Photos': 3, 'Likes': 21, 'Comments': 2},
+              {'Likes': 13, 'Comments': 2, 'Shares': 1},
+              {'Photos': 5, 'Likes': 33, 'Comments': 8, 'Shares': 3},
+              {'Comments': 4, 'Shares': 2},
+              {'Photos': 8, 'Comments': 1, 'Shares': 1},
+              {'Photos': 3, 'Likes': 19, 'Comments': 3}]
+
+total_likes = 0
+
+try:
+    for post in blog_posts:
+        total_likes += post.get('Likes', -1)
+
+except KeyError:
+    pass
+print(total_likes)
